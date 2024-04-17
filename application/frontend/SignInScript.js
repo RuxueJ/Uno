7f066ab68d7cf6110edc8621425e72feb1d8b74b
RuxueJ <rjin@sfsu.edu>
2024-04-16 22:44:16 -0700

    basic log in page

---
+++ application/frontend/SignInScript.js
@@ -0,0 +1,36 @@
+async function login(email, password) {
+  try {
+    const response = await fetch("http://localhost:3000/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Login failed");
+    }
+
+    const data = await response.json();
+
+    // Store the JWT token in local storage for future requests
+    localStorage.setItem("token", data.token);
+    // localStorage.setItem("userId", data.data.id);
+    // localStorage.setItem("username", data.data.userName);
+    // Redirect to the dashboard or perform any other action
+    window.location.href = "dashboard.html"; // Change the URL accordingly
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Login failed. Please try again.");
+  }
+}
+
+document
+  .getElementById("signInForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    login(email, password);
+  });
