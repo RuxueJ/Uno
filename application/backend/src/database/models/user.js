04bf5657a4e77b2e0201bfbe02059d6cb48439cf
guxu11 <guxu19970823@126.com>
2024-05-06 15:22:13 -0700

    fix user model

--- application/backend/src/database/models/user.js
+++ application/backend/src/database/models/user.js
@@ -20,6 +20,12 @@ export default function (sequelize) {
     }
 
     User.init({
+      userId: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+      },
         userName: {
           type: DataTypes.STRING(20),
           allowNull: false,
