84c4cd382347c66372d16ac7cb18ade013bbae36
RuxueJ <rjin@sfsu.edu>
2024-05-10 10:27:10 -0700

    add game room page

--- application/frontend/index.js
+++ application/frontend/index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 8080;
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-    res.redirect('/public/signIn.html');
+app.get("/", function (req, res) {
+  res.redirect("/public/signIn.html");
 });
 
-app.get('/signup', function(req, res) {
-    res.redirect('/public/signUp.html');
+app.get("/signup", function (req, res) {
+  res.redirect("/public/signUp.html");
 });
 
-app.get('/room', function(req, res) {
-    res.redirect('/public/room.html');
+app.get("/lobby", function (req, res) {
+  res.redirect("/public/lobby.html");
+});
+app.get("/game", function (req, res) {
+  res.redirect("/public/game.html");
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
