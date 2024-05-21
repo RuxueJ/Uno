0a598b246bf77ccdb9512ffcdd0954b0ab236a57
RuxueJ <rjin@sfsu.edu>
2024-05-21 14:41:26 -0700

    rename rejoin when user is in the room but disconnected

--- application/frontend/public/gameScript.js
+++ application/frontend/public/gameScript.js
@@ -150,6 +150,8 @@ function startGame() {
   const roomId = urlParams.get("roomId");
   socket.emit("startGame", roomId);
   console.log(`Starting game ${roomId}`);
+  clearDeckMessage();
+  clearStartButton();
 }
 
 function endGame() {
@@ -180,12 +182,11 @@ socket.on("connect", () => {
   }, 500); //needs short delay to make sure the socket is fully connected
 });
 
+socket.emit("reconnectAttempt", userId);
 
-socket.emit('reconnectAttempt', userId)
-
-socket.on('backToLobby', () => {
+socket.on("backToLobby", () => {
   window.location.href = "lobby.html";
-})
+});
 
 function reJoinGame() {
   console.log("rejoining: " + roomId + " for user: " + userId);
@@ -205,18 +206,15 @@ socket.on("newRoomMessage", function (data) {
 socket.on("userJoin", () => {
   console.log("I am in userJoin event");
   getUserInRoom();
-
 });
 
-socket.on('userReconnect', () => {
-  console.log("I am in userReconnect event")
+socket.on("userReconnect", () => {
+  console.log("I am in userReconnect event");
   getUserInRoom();
-  socket.emit('reconnected', roomId)
+  socket.emit("reconnected", roomId);
   clearDeckMessage();
   clearStartButton();
-
-})
-
+});
 
 // Handling "userLeft" event
 socket.on("userLeft", () => {
@@ -235,6 +233,7 @@ socket.on("drawnCards", (data) => {
 
 socket.on("nextTurn", (data) => {
   // check if it is your turn
+  console.log("I am in nextTurn event");
   if (data.nextTurn == userId) {
     showDrawPlayButton();
     showTurn();
@@ -248,6 +247,9 @@ socket.on("playedCard", (data) => {
   
   // top deck card
   console.log(data);
+
+
+  
 });
 
 //=========================startGame====================================
@@ -296,6 +298,7 @@ socket.on("gameStarted", (data) => {
   if (data.nextTurn == userId) {
     showDrawPlayButton();
     showTurn();
+    showDrawPlayButton();
   } else {
     // Get the div element by its ID
     disappearDrawPlayButton();
